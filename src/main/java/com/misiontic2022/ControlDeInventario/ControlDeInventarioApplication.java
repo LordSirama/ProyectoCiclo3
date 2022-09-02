a2a09b0ce41b1dc9af5e6df7ec20f28a801bf611
Javier Molina Barroso <javiermolina.1707@gmail.com>
2022-09-01 20:03:43 -0500

    primer commit

---
+++ src/main/java/com/misiontic2022/ControlDeInventario/ControlDeInventarioApplication.java
@@ -0,0 +1,12 @@
+package com.misiontic2022.ControlDeInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeInventarioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/misiontic2022/ControlDeInventario/ControlDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic2022.ControlDeInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
